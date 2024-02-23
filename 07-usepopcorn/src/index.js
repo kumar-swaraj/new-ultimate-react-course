3c48f947b4775a345e616e9af50405db4c7caa97
kumar-swaraj <kumarswaraj511@gmail.com>
2024-02-24 01:44:02 +0530

    07-usePopcorn 12 how NOT to fetch data in react

--- 07-usepopcorn/src/index.js
+++ 07-usepopcorn/src/index.js
@@ -1,33 +1,34 @@
-import React, { useState } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import './index.css';
-// import App from './App';
+import './index.css';
+import App from './App';
 
-import StarRating from './StarRating';
+// import StarRating from './StarRating';
 
-function Test() {
-  const [movieRating, setMovieRating] = useState(0);
-  return (
-    <div>
-      <StarRating
-        maxRating={7}
-        color="yellowgreen"
-        onSetRating={setMovieRating}
-      />
-      <p>This movie was {movieRating} stars.</p>
-    </div>
-  );
-}
+// function Test() {
+//   const [movieRating, setMovieRating] = useState(0);
+//   return (
+//     <div>
+//       <StarRating
+//         maxRating={7}
+//         color="yellowgreen"
+//         onSetRating={setMovieRating}
+//       />
+//       <p>This movie was {movieRating} stars.</p>
+//     </div>
+//   );
+// }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <StarRating
+    <App />
+
+    {/* <StarRating
       maxRating={5}
       messages={['terrible', 'bad', 'okay', 'good', 'amazing']}
     />
     <StarRating color="red" size={24} defaultRating={3} />
-    <Test />
+    <Test /> */}
   </React.StrictMode>
 );
