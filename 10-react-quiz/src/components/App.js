a350c80f27810ce27acbc432b8e1d1cdb1970cbd
kumar-swaraj <kumarswaraj511@gmail.com>
2024-02-29 03:45:56 +0530

    10-react-quiz 16 restarting a quiz

--- 10-react-quiz/src/components/App.js
+++ 10-react-quiz/src/components/App.js
@@ -49,6 +49,13 @@ function reducer(state, action) {
         highscore:
           state.points > state.highscore ? state.points : state.highscore,
       };
+    case 'restart':
+      return {
+        ...initialState,
+        questions: state.questions,
+        status: 'ready',
+        highscore: state.highscore,
+      };
 
     default:
       throw new Error('Action unknown');
@@ -119,6 +126,7 @@ export default function App() {
             points={points}
             totalPoints={totalPoints}
             highscore={highscore}
+            dispatch={dispatch}
           />
         )}
       </Main>
--- 10-react-quiz/src/components/FinishScreen.js
+++ 10-react-quiz/src/components/FinishScreen.js
@@ -1,4 +1,4 @@
-function FinishScreen({ points, totalPoints, highscore }) {
+function FinishScreen({ points, totalPoints, highscore, dispatch }) {
   const percentage = (points / totalPoints) * 100;
 
   let emoji;
@@ -15,6 +15,13 @@ function FinishScreen({ points, totalPoints, highscore }) {
         {totalPoints} ({Math.ceil(percentage)}%)
       </p>
       <p className="highscore">(Highscore: {highscore} points)</p>
+
+      <button
+        className="btn btn-ui"
+        onClick={() => dispatch({ type: 'restart' })}
+      >
+        Restart quiz
+      </button>
     </>
   );
 }
