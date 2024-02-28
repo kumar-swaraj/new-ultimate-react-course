78e687ccb5f915502fc8718e0c2ee3befa46ccd9
kumar-swaraj <kumarswaraj511@gmail.com>
2024-02-28 19:35:40 +0530

    10-react-quiz 16 loading questions from a fake api

--- 10-react-quiz/src/App.js
+++ 10-react-quiz/src/App.js
@@ -1,7 +1,48 @@
+import { useEffect, useReducer } from 'react';
 import Header from './Header';
 import Main from './Main';
 
+const initialState = {
+  questions: [],
+
+  // 'loading', 'error', 'ready', 'active', 'finished'
+  status: 'loading',
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'dataReceived':
+      return { ...state, questions: action.payload, status: 'ready' };
+    case 'dataFailed':
+      return { ...state, status: 'error' };
+    default:
+      throw new Error('Action unknown');
+  }
+}
+
 export default function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(function () {
+    const controller = new AbortController();
+
+    async function fetchQuestions() {
+      try {
+        const res = await fetch(`http://localhost:8000/questions`, {
+          signal: controller.signal,
+        });
+        const data = await res.json();
+
+        dispatch({ type: 'dataReceived', payload: data });
+      } catch (err) {
+        dispatch({ type: 'dataFailed' });
+        console.error(err.message);
+      }
+    }
+    fetchQuestions();
+    return () => controller.abort();
+  }, []);
+
   return (
     <div className="app">
       <Header />
