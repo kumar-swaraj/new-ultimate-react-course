4395150c93623c463dd0b9730b68d5ddb731daaa
kumar-swaraj <kumarswaraj511@gmail.com>
2024-03-04 21:11:45 +0530

    11-worldwise 17 building the pages

--- 11-worldwise/src/App.jsx
+++ 11-worldwise/src/App.jsx
@@ -1,8 +1,9 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-import Product from './pages/Product';
-import Pricing from './pages/Pricing';
 import Homepage from './pages/Homepage';
+import Pricing from './pages/Pricing';
+import Product from './pages/Product';
+import Login from './pages/Login';
 import AppLayout from './pages/AppLayout';
 import PageNotFound from './pages/PageNotFound';
 
@@ -11,8 +12,9 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Homepage />} />
-        <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
+        <Route path="product" element={<Product />} />
+        <Route path="login" element={<Login />} />
         <Route path="app" element={<AppLayout />} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
