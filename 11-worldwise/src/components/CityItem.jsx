e6a182cc43d8c0c3a9eccea99f312cad737af638
kumar-swaraj <kumarswaraj511@gmail.com>
2024-03-10 23:05:44 +0530

    11-worldwise 17 dynamic routes with url parameters

--- 11-worldwise/src/components/CityItem.jsx
+++ 11-worldwise/src/components/CityItem.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import styles from './CityItem.module.css';
 
 const formatDate = (date) =>
@@ -7,13 +8,15 @@ const formatDate = (date) =>
     year: 'numeric',
   }).format(new Date(date));
 
-function CityItem({ city: { cityName, date, emoji } }) {
+function CityItem({ city: { cityName, date, emoji, id } }) {
   return (
-    <li className={styles.cityItem}>
-      <span className={styles.emoji}>{emoji}</span>
-      <h3 className={styles.name}>{cityName}</h3>
-      <time className={styles.date}>({formatDate(date)})</time>
-      <button className={styles.deleteBtn}>&times;</button>
+    <li>
+      <Link className={styles.cityItem} to={`${id}`}>
+        <span className={styles.emoji}>{emoji}</span>
+        <h3 className={styles.name}>{cityName}</h3>
+        <time className={styles.date}>({formatDate(date)})</time>
+        <button className={styles.deleteBtn}>&times;</button>
+      </Link>
     </li>
   );
 }
