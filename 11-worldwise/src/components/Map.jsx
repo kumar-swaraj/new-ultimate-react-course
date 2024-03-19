6f48813078e64fd05d385f533f22478b721016d5
kumar-swaraj <kumarswaraj511@gmail.com>
2024-03-19 20:51:25 +0530

    11-worldwise 18 setting map position with geolocation

--- 11-worldwise/src/components/Map.jsx
+++ 11-worldwise/src/components/Map.jsx
@@ -9,15 +9,20 @@ import {
   useMapEvents,
 } from 'react-leaflet';
 import { useCities } from '../contexts/CitiesContext';
+import { useGeolocation } from '../hooks/useGeolocation';
+import Button from './Button';
 
 import styles from './Map.module.css';
 
 function Map() {
   const { cities } = useCities();
-
   const [mapPosition, setMapPosition] = useState([40, 0]);
-
   const [searchParams] = useSearchParams();
+  const {
+    isLoading: isLoadingPosition,
+    position: geolocationPosition,
+    getPosition,
+  } = useGeolocation();
 
   const mapLat = searchParams.get('lat');
   const mapLng = searchParams.get('lng');
@@ -29,8 +34,21 @@ function Map() {
     [mapLat, mapLng]
   );
 
+  useEffect(
+    function () {
+      if (geolocationPosition)
+        setMapPosition([geolocationPosition.lat, geolocationPosition.lng]);
+    },
+    [geolocationPosition]
+  );
+
   return (
     <div className={styles.mapContainer}>
+      {!geolocationPosition && (
+        <Button type="position" onClick={getPosition}>
+          {isLoadingPosition ? 'Loading...' : 'Use your position'}
+        </Button>
+      )}
       <MapContainer
         className={styles.map}
         center={mapPosition}
