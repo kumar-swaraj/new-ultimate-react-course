f99cb868db26a639e864758ed2d406d6dad10bea
kumar-swaraj <kumarswaraj511@gmail.com>
2024-03-21 02:23:51 +0530

    11-worldwise 18 adding fake authentication protecting a route

--- 11-worldwise/src/App.jsx
+++ 11-worldwise/src/App.jsx
@@ -1,17 +1,19 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { CitiesProvider } from './contexts/CitiesContext';
+import { AuthProvider } from './contexts/FakeAuthContext';
 
 import Homepage from './pages/Homepage';
 import Pricing from './pages/Pricing';
 import Product from './pages/Product';
 import Login from './pages/Login';
+import ProtectedRoute from './pages/ProtectedRoute';
 import AppLayout from './pages/AppLayout';
 import PageNotFound from './pages/PageNotFound';
+
 import CityList from './components/CityList';
 import CountryList from './components/CountryList';
 import City from './components/City';
 import Form from './components/Form';
-import { CitiesProvider } from './contexts/CitiesContext';
-import { AuthProvider } from './contexts/FakeAuthContext';
 
 function App() {
   return (
@@ -24,7 +26,14 @@ function App() {
             <Route path="pricing" element={<Pricing />} />
             <Route path="product" element={<Product />} />
             <Route path="login" element={<Login />} />
-            <Route path="app" element={<AppLayout />}>
+            <Route
+              path="app"
+              element={
+                <ProtectedRoute>
+                  <AppLayout />
+                </ProtectedRoute>
+              }
+            >
               <Route index element={<Navigate replace to="cities" />} />
               <Route path="cities" element={<CityList />} />
               <Route path="cities/:id" element={<City />} />
